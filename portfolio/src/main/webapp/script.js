da9b33241165b6e59fb90763596cd65a1fc9a918
Alex Najera <alexisnajera123@gmail.com>
2021-06-19 05:02:47 +0000

    Remove unnecesary <h1> in output in servlet. Add show hello button and container to html file. Add async function in js file to fetch a static string. Add gson dependency to pom.xml.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,3 +27,11 @@ function addRandomQuote() {
   quoteContainer.innerText = quote;
 }
 
+async function showHelloWorld() {
+  const responseFromServer = await fetch('/hello');
+  const textFromResponse = await responseFromServer.text();
+ 
+  const dateContainer = document.getElementById('hello-container');
+  dateContainer.innerText = textFromResponse;
+}
+
